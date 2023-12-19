2bcad6d49075c36d3a0cbc6f4f5f166dcda84f0f
Sreejith-k-c <kcsreejith17120@gmail.com>
2023-12-19 15:10:08 +0530

    greenspeed station

--- lib/presentation/view/settings_page/greenspeed_station.dart
+++ lib/presentation/view/settings_page/greenspeed_station.dart
@@ -25,23 +25,38 @@ class _GreenspeedState extends State<Greenspeed> {
                 width: double.infinity,
                 height: 150,
                 decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 216, 216, 216),
+                      color: Color.fromARGB(255, 239, 237, 237),
                       borderRadius: BorderRadius.circular(10),
                     ),
-                child: SfSlider(
-                  min: 0.0,
-                  max: 100.0,
-                  value: _value,
-                  showTicks: true,
-                  showLabels: true,
-                  enableTooltip: true,
-                  minorTicksPerInterval: 1,
-                  onChanged: (dynamic value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
+                child: Column(
+                  children: [
+                    SizedBox(height: 10,),
+                    Text("How much time you need to change the vehicle?",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black)),
+                    SfSlider(
+                      min: 0.0,
+                      max: 10.0,
+                      value: _value,
+                      showTicks: true,
+                      showLabels: true,
+                      enableTooltip: true,
+                      minorTicksPerInterval: 1,
+                      onChanged: (dynamic value) {
+                        setState(() {
+                          _value = value;
+                        });
+                      },
+                    ),
+                     SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Price Estimation",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),),
+                    Text('${_value.round()*150}')
+                          ],
+                  ),
+                  ],
                 ),
+                
               ),
             ),
           ),
@@ -49,10 +64,55 @@ class _GreenspeedState extends State<Greenspeed> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
+              child: Column(
+                children: [
+                  SizedBox(height: 10,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Charge required",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.grey),),
+                    
+                    Text("Charger type",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.grey),)
+                  ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("12 Units",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),),
+                    
+                    Text("AC Type-2",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),)
+                  ],
+                  ),
+                  SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Location",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.grey),),
+                    
+                    Text("Charging slot",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.grey),)
+                  ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("4517 Washington Ave",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),),
+                    
+                    Text("A-2",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),)
+                  ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Manchester,Kentucky 39495",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.black),),                    
+                  ],
+                  ),
+                ],
+              ),
               width: double.infinity,
                 height: 150,
                 decoration: BoxDecoration(
-                       color: const Color.fromARGB(255, 216, 216, 216),
+                       color: Color.fromARGB(255, 239, 237, 237),
                       borderRadius: BorderRadius.circular(10),
                     ),
             ),
